a75cb01550789f7555fecc8d5f5156948fa6435e
julian-pfurtscheller <julian.pfurtscheller@gmx.at>
2026-03-13 21:05:48 +0100

    Fix database config and add DEFAULT_AUTO_FIELD

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 import cloudinary
-import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,11 +57,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
 }
 
 CLOUDINARY_STORAGE = {
@@ -85,3 +83,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
